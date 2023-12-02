1dc8221c353965ae18e4f52c550c660e951ff99b
shariard58 <shariard58@gmail.com>
2023-12-02 10:53:16 +0600

    async code started

--- index.js
+++ index.js
@@ -2,5 +2,8 @@
 import './style.css';
 
 // Write Javascript code!
+
+
+
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>Shariar</h1>`;
